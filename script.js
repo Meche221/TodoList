27378958021d30d4fac1ae33acbbbb8edde976c7
Meche221 <binaymeche22@gmail.com>
2025-07-08 14:16:48 +0545

    some sorta things added before taking a break

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const todoList = []; 
+const todoList = ['make dinner', 'wash dishes']; 
+
+renderTodoList();
+
+function renderTodoList(){
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+}
 
 function addTodo() {
  const inputElement= document.querySelector('#js-name-input');
@@ -8,4 +25,28 @@ function addTodo() {
  console.group(todoList);
 
  inputElement.value = " ";
+
+ renderTodoList();
 }
+
+
+
+
+
+
+
+// const nums = [1, 2, 3, 4];
+// let total = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//   const num = nums[i];
+//   total += num;
+// }
+// console.log(total);
+
+// const numsDoubled = [];
+// for(let i = 0; i< nums.length; i++){
+//   const num = nums[i];
+//   numsDoubled.push(num * 2);
+// }
+// console.log(numsDoubled);
